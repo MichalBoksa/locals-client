80c42763b48ceb8e5b23bd834b0b9c775e266edf
unknown <ul0249604@edu.uni.lodz.eu>
2023-11-27 20:02:24 +0100

    added guide registration view

---
+++ app/src/main/java/com/example/locals/RegisterGuideFragment.java
@@ -0,0 +1,19 @@
+package com.example.locals;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class RegisterGuideFragment extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register_guide, container, false);
+    }
+}
